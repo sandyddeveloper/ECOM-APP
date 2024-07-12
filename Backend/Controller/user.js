597c6af077edf027cdd6b41ff9c7b1fcab60d084
sandyddeveloper <sandyddeveloper@gmail.com>
2024-07-12 22:19:31 +0530

    login

--- Backend/Controller/user.js
+++ Backend/Controller/user.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import sendMail from "../middleware/sendmail.js";
 
 // New user Registration
-const RegistrationUser = async (req, res) => {
+export  const RegistrationUser = async (req, res) => {
     try {
         console.log(req.body);
         const { name, email, password, contact } = req.body;
@@ -60,7 +60,7 @@ export const verifyUser = async (req, res) => {
             contact: verify.user.contact,
         });
 
-        res.status(200).json({
+        return res.status(200).json({
             message: "User verified and registered successfully",
         });
     } catch (error) {
@@ -70,4 +70,40 @@ export const verifyUser = async (req, res) => {
     }
 };
 
-export default RegistrationUser;
+//Login User
+export const  loginUser = async (req, res)=>{
+    try {
+         const {email, password } = req.body;
+        
+         // Check User Email Address
+         const user = await User.findOneAndUpdate({email})
+         if(!user){
+            return res.status(400).json({
+                message:"Invalid Email or Password",
+            })
+         }
+
+         //Check Password 
+         const matchPassword = await bcrypt.compare(password, user.password);
+         if(!matchPassword){
+            return res.status(400).json({
+                message:"Invalid Email & Password",
+         });
+        }
+
+        //Generate Signed Token
+        const token = jwt.sign({id : user.id})
+
+
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message,
+        });
+    }
+};
+
+// export default {RegistrationUser, loginUser}
+
+
+//
+//14:15 / 49:05
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,10 +1,11 @@
 
 import express from "express";
-import RegistrationUser, { verifyUser } from '../Controller/user.js';
+import { RegistrationUser, verifyUser, loginUser } from '../Controller/user.js';
 
 const router = express.Router();
 
 router.post('/user/register', RegistrationUser);
 router.post('/user/verify', verifyUser);
+router.post('/user/login', loginUser);
 
 export default router;
